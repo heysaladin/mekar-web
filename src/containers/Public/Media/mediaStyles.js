600154d0d7d63cdc6f0e488d8c412f0d225283bc
saladinid <didien.geonk@gmail.com>
2017-05-07 09:37:53 +0700

    clean up

--- src/containers/Public/Media/mediaStyles.js
+++
@@ -1,7 +0,0 @@
-export default {
-  grid: {
-    width: '50%',
-    margin: '0 5px 10px 5px',
-    border: '1px solid #C8C8C8'
-  }
-};
--- src/containers/Public/Produk/Loan/loanSuccessStyles.js
+++
@@ -1,7 +0,0 @@
-import Layout from '../../App/Layout';
-export default {
-  title: {
-    fontSize: Layout.fontSize + 3,
-    textTransform: 'uppercase'
-  }
-};
